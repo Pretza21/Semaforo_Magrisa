bb1f0f659d05fecf14b30c370bf4018f611281f5
Angel Pretzanzin <aspretzanzin@gmail.com>
2020-11-10 16:15:26 -0600

    Mi Primer Commitbash: :wq: command not found bash: wq: command not found bash: qw: command not found bash: qw: command not found
    bash: x: command not found

---
+++ Semaforo_Magrisa_1.0.ino
@@ -0,0 +1,26 @@
+#include <SoftwareSerial.h>
+
+void setup() {
+
+}
+
+void loop() {
+
+
+  int No_random = random(0,100);
+
+  if (No_random >= 10){
+    //Enciende los LED Verdes y el buzzer por 1.5 segundos
+    digitalWrite(10,HIGH);
+    digitalWrite(11,HIGH);
+    delay(1500);
+
+    //Apaga el LED Verde y el Buzzer
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    
+    }
+   
+
+
+}
